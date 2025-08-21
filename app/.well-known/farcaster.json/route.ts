d2bc6e8edc50a6731f38fac825d8682037986f7f
Ofem Eteng <ofemeteng@gmail.com>
2025-08-21 09:37:32 +0100

    add base builder to farcaster.json

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -39,5 +39,8 @@ export async function GET() {
       ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
       ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
     }),
+    "baseBuilder": {
+      "allowedAddresses": ["0x875e1C39743E69E012c0d5EE635cD4644046Da72"]
+    },
   });
 }
